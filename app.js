5412f027a86e5c46bc5b8d5eed8a3eb243f2b101
jjac91 <denjand91@gmail.com>
2022-01-04 00:26:22 -0500

    finished tasks

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-let idx = 0
-searchButton.addEventListener("click",function(e){
-    let term = document.getElementById("search-term").value
-    return term
+const gifArea = document.querySelector('#gif-area')
+const sbtn = document.querySelector("#searchButton")
+const input = document.querySelector("#search-term")
+const rbtn = document.querySelector('#clear')
+sbtn.addEventListener("click", function(e){
+    e.preventDefault()
+    getGif()
+    input.value = ""
 })
 
-$("form").on("submit",async function(e){
+rbtn.addEventListener("click", function(e){
     e.preventDefault()
-    let term = $("#search-term").val()
-    
-    let response = await axios.get ("https://api.giphy.com/v1/gifs/search",{params: {api_key: "6ELU5n4uDHVGr8fGo0Gif8V30qRSFnX8", q: term, limit: 1, offset:0, rating:"g", lang:"en" }})
-    console.log(response.data[0])
-    $("#gif-area").append(response.data[0])
+    gifArea.innerHTML =""
 })
-//async function getGif(){let response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=6ELU5n4uDHVGr8fGo0Gif8V30qRSFnX8&q=${term}&limit=1&offset=0&rating=g&lang=en`)console.log(response)
-//
-//
+
+async function getGif(){
+    let term = input.value
+    
+    let response = await axios.get ("https://api.giphy.com/v1/gifs/search",{params: {api_key: "6ELU5n4uDHVGr8fGo0Gif8V30qRSFnX8", q: term}})
+    const url = response.data.data[0].images.original.url
+    console.log(url)
+    makeImg(url)
+}
+
+function makeImg(url){
+    const img = document.createElement("img")
+    img.setAttribute("src", url)
+    console.log(img)
+    console.log(gifArea)
+    gifArea.append(img)
+}
---
+++ garbage.js
@@ -0,0 +1,22 @@
+const gifArea = $("#gif-area")
+$("form").on("submit",async function(e){
+    e.preventDefault()
+    let term = $("#search-term").val()
+    
+    let response = await axios.get ("https://api.giphy.com/v1/gifs/search",{params: {api_key: "6ELU5n4uDHVGr8fGo0Gif8V30qRSFnX8", q: term}})
+    console.log(response)
+    const url = response.data.data[0].images.original.url
+    makeImg(url)
+})
+
+function makeImg(url){
+    const img = $("<img>", {src : url})
+    console.log(img)
+    gifArea.append(img)
+    gifArea.append("I AM APPENDED")
+}
+
+$("#clear").on("click", function(e){
+    e.preventDefault()
+    gifArea.empty()
+})
